e76d30725d13f06a3bfc8d3f7d353bd274c4a448
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-07 10:58:48 +0200

    added basic shadcn components to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function HomePage() {
-  return <h1 className='text-3xl font-bold'>Armast Store</h1>;
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Armast Store</h1>
+    </div>
+  );
 }
 
 export default HomePage;
