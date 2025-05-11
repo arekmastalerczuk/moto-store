46cd04205b57482eaa8509288a3b3acbbe7bf2d4
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-11 18:41:08 +0200

    implemented basic CartButton component

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+import { FiShoppingCart } from 'react-icons/fi';
 
-function CartButton() {
-  return <div>CartButton</div>;
+async function CartButton() {
+  // TODO: dynamic numItemsInCart
+  const numItemsInCart = 2;
+
+  return (
+    <Button
+      variant='outline'
+      size='icon'
+      className='relative flex items-center justify-center'
+      asChild
+    >
+      <Link href='/cart'>
+        <FiShoppingCart />
+        {numItemsInCart > 0 && (
+          <span className='absolute -right-3 -top-3 flex size-6 items-center justify-center rounded-full bg-primary text-xs text-primary-foreground'>
+            {numItemsInCart}
+          </span>
+        )}
+      </Link>
+    </Button>
+  );
 }
 
 export default CartButton;
