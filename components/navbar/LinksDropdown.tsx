8f8e41da95d996a14f1ce79a6402ef7e18826829
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-12 14:18:02 +0200

    added LinksDropdown basic menu

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu';
+import { Button } from '../ui/button';
+
+import { FiMenu } from 'react-icons/fi';
+import { navLinks } from '@/utils/links';
+import Link from 'next/link';
 
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='outline' className='flex max-w-24 gap-4'>
+          <FiMenu className='size-6' />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-32' align='start' sideOffset={14}>
+        {navLinks.map((link) => (
+          <DropdownMenuItem key={link.href}>
+            <Link href={link.href} className='w-full'>
+              {link.label}
+            </Link>
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
