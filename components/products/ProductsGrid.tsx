c889e2b09c3fa4e229f64c811eb115e557023a68
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-13 16:23:46 +0200

    added ProductsGrid component

--- components/products/ProductsGrid.tsx
+++ components/products/ProductsGrid.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Product } from '@prisma/client';
+import SingleGridProduct from './SingleGridProduct';
 
-function ProductsGrid() {
-  return <div>ProductsGrid</div>;
+type Props = {
+  products: Product[];
+};
+
+function ProductsGrid({ products }: Props) {
+  return (
+    <div className='grid gap-4 pt-12 md:grid-cols-2 lg:grid-cols-3'>
+      {products.map((product) => {
+        return <SingleGridProduct key={product.id} product={product} />;
+      })}
+    </div>
+  );
 }
 
 export default ProductsGrid;
