9c5e666cbca78fb40c06a5ecea70d64755173011
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-13 16:22:19 +0200

    added basic FavoriteToggleButton component

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Button } from '../ui/button';
+import { FaHeart } from 'react-icons/fa';
 
-function FavoriteToggleButton() {
-  return <div>FavoriteToggleButton</div>;
+type Props = {
+  productId: string;
+};
+
+function FavoriteToggleButton({ productId }: Props) {
+  return (
+    <Button size='icon' variant='outline' className='cursor-pointer p-2'>
+      <FaHeart />
+    </Button>
+  );
 }
 
 export default FavoriteToggleButton;
