2b3f348e1807506742dfe964430a537d4ead2c53
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-17 12:59:26 +0200

    added optional className prop into section title

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Separator } from '@/components/ui/separator';
+import { cn } from '@/lib/utils';
 
 type Props = {
   title: string;
+  className?: string;
 };
 
-function SectionTitle({ title }: Props) {
+function SectionTitle({ title, className }: Props) {
   return (
     <>
-      <h2 className='mb-8 text-3xl font-medium capitalize tracking-wider'>
+      <h2
+        className={cn(
+          'mb-8 text-3xl font-medium capitalize tracking-wider',
+          className,
+        )}
+      >
         {title}
       </h2>
       <Separator />
