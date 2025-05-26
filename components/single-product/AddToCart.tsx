57e1f507693bb1daab0fb3064c4e0fc06268666f
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-26 12:49:55 +0200

    added button 'add to cart'

---
+++ components/single-product/AddToCart.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from '../ui/button';
+
+type Props = {
+  productId: string;
+};
+
+function AddToCart({ productId }: Props) {
+  return (
+    <Button size='lg' className='mt-8 capitalize'>
+      add to cart
+    </Button>
+  );
+}
+
+export default AddToCart;
