054b1d1309bfc6de3643c77aca59fd7c96d3bfa0
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-05-26 12:48:00 +0200

    added action to fetch single product

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,4 +1,5 @@
 import prisma from '@/utils/db';
+import { redirect } from 'next/navigation';
 
 export const fetchFeaturedProducts = async () => {
   return await prisma.product.findMany({
@@ -20,4 +21,18 @@ export const fetchAllProducts = async ({ search = '' }: { search: string }) => {
       createAt: 'desc',
     },
   });
+};
+
+export const fetchSingleProduct = async (productId: string) => {
+  const foundProduct = await prisma.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+
+  if (!foundProduct) {
+    return redirect('/products');
+  }
+
+  return foundProduct;
 };
