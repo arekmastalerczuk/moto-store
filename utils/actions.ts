6057b8a9a06aefcc585ecfacb32576db1d55c82c
Arkadiusz Mastalerczuk <arekmas@gmail.com>
2025-09-16 14:06:08 +0200

    add order model, add helper function: format date, add actions: create order, fetch user order and fetch all users orders for admin

--- utils/actions.ts
+++ utils/actions.ts
@@ -638,7 +638,63 @@ export const createOrderAction = async (
   prevState: any,
   formData: FormData,
 ) => {
-  return {
-    message: "order created",
-  };
+  const user = await getAuthUser();
+
+  try {
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+
+    const order = await prisma.order.create({
+      data: {
+        clerkId: user.id,
+        products: cart.numItemsInCart,
+        orderTotal: cart.orderTotal,
+        tax: cart.tax,
+        shipping: cart.shipping,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+
+    await prisma.cart.delete({
+      where: {
+        id: cart.id,
+      },
+    });
+  } catch (error) {
+    return renderError(error);
+  }
+
+  redirect("/orders");
+};
+
+export const fetchUserOrders = async () => {
+  const user = await getAuthUser();
+  const orders = await prisma.order.findMany({
+    where: {
+      clerkId: user.id,
+      isPaid: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return orders;
+};
+
+export const fetchAllOrdersForAdmin = async () => {
+  await getAdminUser();
+
+  const orders = await prisma.order.findMany({
+    where: {
+      isPaid: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return orders;
 };
